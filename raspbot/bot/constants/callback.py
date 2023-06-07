b9e73ff1e962e583be55494b30e6707e5b3d69aa
Ilya Kutilin <ilyakutilin@yandex.ru>
2023-06-07 16:50:31 +0300

    Searching for points is now broken down into exact vs. inexact

--- raspbot/bot/constants/callback.py
+++ raspbot/bot/constants/callback.py
@@ -1,9 +1,12 @@
 from aiogram.filters.callback_data import CallbackData
 
-MISSING_POINT = "missing_point_{dep_or_dest}"
-
 
 class PointsCallbackFactory(CallbackData, prefix="pointselect"):
     is_departure: bool
     is_station: bool
     point_id: int
+
+
+class MissingPointCallbackFactory(CallbackData, prefix="missing_point"):
+    is_departure: bool
+    exact: bool
