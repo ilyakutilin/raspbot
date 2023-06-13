894c8ac3b5ede1dc4a8be5975e4da545bb92cd5e
Ilya Kutilin <ilyakutilin@yandex.ru>
2023-06-13 16:12:38 +0300

    Documenting apicalls/base.py

--- raspbot/apicalls/base.py
+++ raspbot/apicalls/base.py
@@ -14,27 +14,42 @@ logger = configure_logging(__name__)
 async def get_response(
     endpoint: str, headers: dict[str, str | bytes | None]
 ) -> dict | None:
-    """Sends a request to the server and returns a response."""
+    """
+    Отправляет запрос на сервер и возвращает ответ.
+
+    Принимает на вход:
+        endpoint (строка): URL адрес (эндпоинт), по которому нуэно перейти;
+        headers (словарь): Хедеры (напр. токен аутентификации для API).
+
+    Вызывает исключения:
+        EmptyHeadersError: вызывается при отсутствии хедеров;
+        APIStatusCodeError: вызывается при плохом статус коде;
+        APIConnectionError: вызывается при ошибках соединения.
+
+    Возвращает:
+        словарь или None: При получении ответа в виде JSON конвертирует его в словарь;
+        в противном случае возвращает None.
+    """
     if headers["Authorization"] is None:
-        raise exc.EmptyHeadersError("Authorization key in the headers is missing.")
+        raise exc.EmptyHeadersError("Отсутствует ключ авторизации в хедере.")
     async with aiohttp.ClientSession() as session:
         try:
-            logger.debug(f"Sending request to API endpoint {endpoint}.")
+            logger.debug(f"Отправляю запрос на адрес {endpoint}.")
             response = await session.get(url=endpoint, headers=headers)
             if response.status != HTTPStatus.OK:
                 raise exc.APIStatusCodeError(
-                    f"Endpoint {endpoint} is unavailable - status: "
+                    f"Адрес {endpoint} недоступен - статус: "
                     f"{response.status} "
                     f"{HTTPStatus(response.status).phrase}. "
                 )
         except Exception as e:
             raise exc.APIConnectionError(
-                f"An error occurred while connecting to endpoint {endpoint}. "
-                f"Headers: {headers}. Error description: {e}"
+                f"При соединении с эндпоинтом {endpoint} возникла ошибка. "
+                f"Хедеры: {headers}. Описание ошибки: {e}"
             ) from e
         else:
             logger.debug(
-                f"Request to API endpoint {endpoint} has been successfully "
-                "completed and the response received."
+                f"Запрос на эндпоинт {endpoint} был успешно осуществлен, "
+                "получен ответ."
             )
             return await response.json(content_type=None)
