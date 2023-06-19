42a0733e6bb13be4000cd18c5cd25324c967905e
Ilya Kutilin <ilyakutilin@yandex.ru>
2023-06-19 10:20:37 +0300

    Re-arrange router imports

---
+++ raspbot/bot/__init__.py
@@ -0,0 +1,3 @@
+from raspbot.bot.routes.handlers import router as routes_router  # noqa
+from raspbot.bot.start import router as start_router  # noqa
+from raspbot.bot.users.handlers import router as users_router  # noqa
