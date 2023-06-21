e664f4ffcbf4f913ff6a13b4ae3d67be01b56abf
Ilya Kutilin <ilyakutilin@yandex.ru>
2023-06-21 10:13:31 +0300

    Added @log decorator

--- raspbot/core/logging.py
+++ raspbot/core/logging.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -25,3 +26,38 @@ def configure_logging(name: str, level: int = logging.DEBUG) -> logging.Logger:
     stream_handler.setLevel(settings.LOG_STREAM_LEVEL)
     logger.addHandler(stream_handler)
     return logger
+
+
+# Декоратор
+
+
+def get_default_logger():
+    return logging.get_logger(__name__)
+
+
+def log(_func=None, *, my_logger: logging.Logger = None):
+    def decorator_log(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if my_logger is None:
+                logger = get_default_logger()
+            else:
+                logger = my_logger
+            args_repr = [repr(a) for a in args]
+            kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+            signature = ", ".join(args_repr + kwargs_repr)
+            logger.debug(f"Функция {func.__name__} вызвана с аргументами {signature}.")
+            try:
+                result = func(*args, **kwargs)
+                return result
+            except Exception as e:
+                logger.exception(
+                    f"В функции {func.__name__} вызвано исключение: {str(e)}"
+                )
+                raise e
+
+        return wrapper
+
+    if _func is None:
+        return decorator_log
+    return decorator_log(_func)
