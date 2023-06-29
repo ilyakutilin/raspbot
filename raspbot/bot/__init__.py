26c5b5321fa9c38f435ce181682e6e15bdf43ce9
Ilya Kutilin <ilyakutilin@yandex.ru>
2023-06-29 11:32:53 +0300

    Add the timetable router to bot

--- raspbot/bot/__init__.py
+++ raspbot/bot/__init__.py
@@ -1,3 +1,4 @@
 from raspbot.bot.routes.handlers import router as routes_router  # noqa
 from raspbot.bot.start import router as start_router  # noqa
+from raspbot.bot.timetable.handlers import router as timetable_router  # noqa
 from raspbot.bot.users.handlers import router as users_router  # noqa
