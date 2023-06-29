94c69434ad7dccfd7012584c4032923a679629cb
Ilya Kutilin <ilyakutilin@yandex.ru>
2023-06-29 15:33:56 +0300

    Fix circular import

--- raspbot/db/models.py
+++ raspbot/db/models.py
@@ -1,3 +1,2 @@
-from .routes.models import Route  # noqa
 from .stations.models import Country, Point, PointTypeEnum, Region  # noqa
-from .users.models import Favorite, Recent, User  # noqa
+from .users.models import Favorite, Recent, Route, User  # noqa
