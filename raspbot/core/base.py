0cad0bb8a9f301608c6daa424adcad76d472d814
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-04-23 11:17:56 +0300

    Autofixes by pre-commit run --all-files

--- raspbot/core/base.py
+++ raspbot/core/base.py
@@ -1,3 +1,4 @@
 """Imports of class Base and all models for Alembic."""
+
 from raspbot.db.base import Base  # noqa
 from raspbot.db.models import Country, Point, Recent, Region, Route, User  # noqa
