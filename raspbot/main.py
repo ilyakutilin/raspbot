ced02f00798abd143f4d195fb1c3a25606f66a72
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-04-24 10:21:07 +0300

    Adding project folder to sys.path when starting the bot

--- raspbot/main.py
+++ raspbot/main.py
@@ -1,6 +1,14 @@
 import asyncio
+import os
+import sys
 
-from raspbot.bot.bot import start_bot
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+from raspbot.bot.bot import start_bot  # noqa
 
 if __name__ == "__main__":
     asyncio.run(start_bot())
