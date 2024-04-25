5676d88df2e90c20d24ff3fcf239d87d8d7c69f2
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-04-25 12:18:48 +0300

    Separate actual handlers from utils functions in timetables

--- raspbot/bot/timetable/handlers.py
+++ raspbot/bot/timetable/handlers.py
@@ -6,11 +6,10 @@ from aiogram.fsm.context import FSMContext
 from raspbot.bot.constants import callback as clb
 from raspbot.bot.constants import messages as msg
 from raspbot.bot.constants import states
-from raspbot.bot.timetable import keyboards as kb
+from raspbot.bot.timetable import utils
 from raspbot.core import exceptions as exc
 from raspbot.core.logging import configure_logging
 from raspbot.db.models import Recent, Route
-from raspbot.db.routes.schema import ThreadResponse
 from raspbot.services.deptime import get_uid_by_time
 from raspbot.services.routes import RouteRetriever
 from raspbot.services.timetable import Timetable
@@ -24,47 +23,6 @@ router = Router()
 route_retriever = RouteRetriever()
 
 
-# TODO: the two functions below can be merged
-async def process_today_timetable_callback(
-    callback: types.CallbackQuery,
-    state: FSMContext,
-    timetable_obj: Timetable,
-):
-    """Answers the callback based on the provided Timetable object for today."""
-    timetable_obj_msgs: tuple = await timetable_obj.msg
-    for part in timetable_obj_msgs:
-        await callback.message.answer(
-            text=part,
-            reply_markup=await kb.get_today_departures_keyboard(
-                timetable_obj=timetable_obj
-            ),
-            parse_mode="HTML",
-        )
-    await callback.answer()
-    await state.update_data(timetable_obj=timetable_obj)
-    await state.set_state(states.TimetableState.exact_departure_info)
-
-
-async def process_date_timetable_callback(
-    callback: types.CallbackQuery,
-    state: FSMContext,
-    timetable_obj: Timetable,
-):
-    """Answers the callback based on the provided Timetable object for certain date."""
-    timetable_obj_msgs = await timetable_obj.msg
-    for part in timetable_obj_msgs:
-        await callback.message.answer(
-            text=part,
-            reply_markup=await kb.get_date_departures_keyboard(
-                route_id=timetable_obj.route.id
-            ),
-            parse_mode="HTML",
-        )
-    await callback.answer()
-    await state.update_data(timetable_obj=timetable_obj)
-    await state.set_state(states.TimetableState.exact_departure_info)
-
-
 @router.callback_query(clb.GetTimetableCallbackFactory.filter())
 async def show_closest_departures_callback(
     callback: types.CallbackQuery,
@@ -75,60 +33,8 @@ async def show_closest_departures_callback(
     recent: Recent = await update_recent(recent_id=callback_data.recent_id)
     route: Route = await route_retriever.get_route_from_db(route_id=recent.route_id)
     timetable_obj = Timetable(route=route, limit=settings.CLOSEST_DEP_LIMIT)
-    await process_today_timetable_callback(
+    await utils.process_today_timetable_callback(
         callback=callback, state=state, timetable_obj=timetable_obj
-    )
-
-
-async def get_timetable_object_from_state(state: FSMContext) -> Timetable | None:
-    """Get the timetable object from the FSM Context state dictionary.
-
-    Args:
-        state: current FSMContext
-
-    Returns:
-        Timetable object or None
-
-    """
-    user_data: dict = await state.get_data()
-    try:
-        timetable_obj: Timetable = user_data["timetable_obj"]
-    except TypeError as e:
-        logger.error(f"user_data is not a dict: {e}")
-        return None
-    except KeyError:
-        logger.error("There is no 'timetable_obj' key in the user_data dict.")
-        return None
-    return timetable_obj
-
-
-async def show_dep_info(
-    timetable_obj: Timetable, uid: str, message: types.Message, full_kb: bool = True
-) -> None:
-    """Answers the message with the departure info.
-
-    :full_kb: if True, the keyboard that is added to the message contains buttons
-              for the upcoming departures and 'Tomorrow' button. If False, the keyboard
-              contains only one button for the other date.
-    """
-    timetable = await timetable_obj.timetable
-    try:
-        dep_info: ThreadResponse = next(dep for dep in timetable if dep.uid == uid)
-    except StopIteration:
-        # TODO: Complete error handling
-        logger.error("StopIteration error")
-    msg_obj = msg.ThreadInfo(thread=dep_info)
-    if full_kb:
-        reply_markup = await kb.get_separate_departure_keyboard(
-            this_departure=dep_info,
-            timetable_obj=timetable_obj,
-        )
-    else:
-        reply_markup = kb.get_other_date_keyboard(route_id=timetable_obj.route.id)
-    await message.answer(
-        text=str(msg_obj),
-        reply_markup=reply_markup,
-        parse_mode="HTML",
     )
 
 
@@ -139,9 +45,11 @@ async def show_departure_callback(
     state: FSMContext,
 ):
     """User: clicks on departure time. Bot: here's the departure info."""
-    timetable_obj: Timetable = await get_timetable_object_from_state(state=state)
+    timetable_obj: Timetable = await utils.get_timetable_object_from_state(state=state)
     uid: str = callback_data.uid
-    await show_dep_info(timetable_obj=timetable_obj, uid=uid, message=callback.message)
+    await utils.show_dep_info(
+        timetable_obj=timetable_obj, uid=uid, message=callback.message
+    )
     await callback.answer()
 
 
@@ -159,7 +67,7 @@ async def show_till_the_end_of_the_day_callback(
 ):
     """User: clicks on the button to see full timetable for today. Bot: here you go."""
     route_id: int = callback_data.route_id
-    timetable_obj: Timetable = await get_timetable_object_from_state(state=state)
+    timetable_obj: Timetable = await utils.get_timetable_object_from_state(state=state)
     timetable_obj = timetable_obj.unlimit()
     if timetable_obj.route.id != route_id:
         route: Route = await route_retriever.get_route_from_db(route_id=route_id)
@@ -169,7 +77,7 @@ async def show_till_the_end_of_the_day_callback(
         "This is what I pass to the process_timetable_callback function after "
         f"unlimiting: {len(timetable)}, last departure: {timetable[-1]}"
     )
-    await process_today_timetable_callback(
+    await utils.process_today_timetable_callback(
         callback=callback, state=state, timetable_obj=timetable_obj
     )
 
@@ -182,7 +90,7 @@ async def select_departure_info_by_text(message: types.Message, state: FSMContex
         message: user input
         state: the current FSM state
     """
-    timetable_obj: Timetable = await get_timetable_object_from_state(state=state)
+    timetable_obj: Timetable = await utils.get_timetable_object_from_state(state=state)
     timetable = await timetable_obj.timetable
     logger.debug(f"timetable_obj has {len(timetable)} elements.")
     try:
@@ -190,7 +98,7 @@ async def select_departure_info_by_text(message: types.Message, state: FSMContex
             user_raw_time_input=message.text, timetable_obj=timetable_obj
         )
         logger.debug(f"uid is {uid}")
-        await show_dep_info(
+        await utils.show_dep_info(
             timetable_obj=timetable_obj, uid=uid, message=message, full_kb=False
         )
     except exc.InvalidDataError as e:
@@ -208,7 +116,7 @@ async def show_tomorrow_timetable_callback(
     route: Route = await route_retriever.get_route_from_db(route_id=route_id)
     tomorrow = dt.date.today() + dt.timedelta(days=1)
     timetable_obj = Timetable(route=route, date=tomorrow)
-    await process_date_timetable_callback(
+    await utils.process_date_timetable_callback(
         callback=callback, state=state, timetable_obj=timetable_obj
     )
 
---
+++ raspbot/bot/timetable/utils.py
@@ -0,0 +1,104 @@
+from aiogram import types
+from aiogram.fsm.context import FSMContext
+
+from raspbot.bot.constants import messages as msg
+from raspbot.bot.constants import states
+from raspbot.bot.timetable import keyboards as kb
+from raspbot.core.logging import configure_logging
+from raspbot.db.routes.schema import ThreadResponse
+from raspbot.services.timetable import Timetable
+
+logger = configure_logging(name=__name__)
+
+
+# TODO: the two functions below can be merged
+async def process_today_timetable_callback(
+    callback: types.CallbackQuery,
+    state: FSMContext,
+    timetable_obj: Timetable,
+):
+    """Answers the callback based on the provided Timetable object for today."""
+    timetable_obj_msgs: tuple = await timetable_obj.msg
+    for part in timetable_obj_msgs:
+        await callback.message.answer(
+            text=part,
+            reply_markup=await kb.get_today_departures_keyboard(
+                timetable_obj=timetable_obj
+            ),
+            parse_mode="HTML",
+        )
+    await callback.answer()
+    await state.update_data(timetable_obj=timetable_obj)
+    await state.set_state(states.TimetableState.exact_departure_info)
+
+
+async def process_date_timetable_callback(
+    callback: types.CallbackQuery,
+    state: FSMContext,
+    timetable_obj: Timetable,
+):
+    """Answers the callback based on the provided Timetable object for certain date."""
+    timetable_obj_msgs = await timetable_obj.msg
+    for part in timetable_obj_msgs:
+        await callback.message.answer(
+            text=part,
+            reply_markup=await kb.get_date_departures_keyboard(
+                route_id=timetable_obj.route.id
+            ),
+            parse_mode="HTML",
+        )
+    await callback.answer()
+    await state.update_data(timetable_obj=timetable_obj)
+    await state.set_state(states.TimetableState.exact_departure_info)
+
+
+async def get_timetable_object_from_state(state: FSMContext) -> Timetable | None:
+    """Get the timetable object from the FSM Context state dictionary.
+
+    Args:
+        state: current FSMContext
+
+    Returns:
+        Timetable object or None
+
+    """
+    user_data: dict = await state.get_data()
+    try:
+        timetable_obj: Timetable = user_data["timetable_obj"]
+    except TypeError as e:
+        logger.error(f"user_data is not a dict: {e}")
+        return None
+    except KeyError:
+        logger.error("There is no 'timetable_obj' key in the user_data dict.")
+        return None
+    return timetable_obj
+
+
+async def show_dep_info(
+    timetable_obj: Timetable, uid: str, message: types.Message, full_kb: bool = True
+) -> None:
+    """Answers the message with the departure info.
+
+    :full_kb: if True, the keyboard that is added to the message contains buttons
+              for the upcoming departures and 'Tomorrow' button. If False, the keyboard
+              contains only one button for the other date.
+    """
+    timetable = await timetable_obj.timetable
+    try:
+        dep_info: ThreadResponse = next(dep for dep in timetable if dep.uid == uid)
+    except StopIteration:
+        # TODO: Complete error handling
+        logger.error("StopIteration error")
+    msg_obj = msg.ThreadInfo(thread=dep_info)
+    if full_kb:
+        reply_markup = await kb.get_separate_departure_keyboard(
+            this_departure=dep_info,
+            timetable_obj=timetable_obj,
+        )
+    else:
+        reply_markup = kb.get_other_date_keyboard(route_id=timetable_obj.route.id)
+    await message.answer(
+        text=str(msg_obj),
+        reply_markup=reply_markup,
+        parse_mode="HTML",
+    )
