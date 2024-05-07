567f7cd60b467ab827c315329cc4ed87e4b8683a
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-05-07 13:42:00 +0300

    Re-write bot starting and closing logic

--- raspbot/bot/bot.py
+++ raspbot/bot/bot.py
@@ -6,7 +6,6 @@ from aiogram.client.telegram import TEST
 
 from raspbot.bot import routes_router, start_router, timetable_router, users_router
 from raspbot.core.logging import configure_logging
-from raspbot.db.stations.schedule import start_update_monitoring
 from raspbot.settings import settings
 
 logger = configure_logging(name=__name__)
@@ -15,22 +14,25 @@ logger = configure_logging(name=__name__)
 custom_session = AiohttpSession(api=TEST)
 
 
-async def start_bot(test: bool = False):
-    """Starts the bot."""
+def get_bot(test: bool = False) -> Bot:
+    """Returns the bot."""
     main_bot = Bot(token=settings.TELEGRAM_TOKEN)
     test_bot = Bot(token=settings.TELEGRAM_TESTENV_TOKEN, session=custom_session)
     bot = test_bot if test else main_bot
+    return bot
 
+
+async def start_bot(bot: Bot, handle_signals: bool = True):
+    """Starts the bot."""
     dp = Dispatcher()
-
     dp.include_routers(users_router, start_router, routes_router, timetable_router)
 
     await bot.delete_webhook(drop_pending_updates=True)
 
-    await start_update_monitoring()
-
-    logger.info("Starting the {} bot.".format("test" if test else ""))
-    await dp.start_polling(bot)
+    logger.info(
+        "Starting the {} bot.".format("test" if bot.session.api == TEST else "")
+    )
+    await dp.start_polling(bot, handle_signals=handle_signals)
 
     logger.info("Bot stopped.")
 
--- raspbot/db/stations/schedule.py
+++ raspbot/db/stations/schedule.py
@@ -1,7 +1,7 @@
 import asyncio
 from datetime import datetime, timedelta, timezone
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler, BaseScheduler
 from sqlalchemy import func, select
 
 from raspbot.core.logging import configure_logging
@@ -13,13 +13,13 @@ from raspbot.settings import settings
 logger = configure_logging(__name__)
 
 
-async def _check_last_station_db_update(
+async def check_last_station_db_update(
     days_between_updates: int = settings.DAYS_BETWEEN_STATIONS_DB_UPDATE,
 ) -> None:
     """Checks when the stations DB was last updated."""
     logger.info("Starting to check when the stations DB was last updated.")
     async with async_session_factory() as session:
-        query = await session.execute(select(func.max(LastUpdatedORM.updated)))
+        query = await session.execute(select(func.max(LastUpdatedORM.created_at)))
         last_updated = query.scalar()
         if not last_updated:
             logger.info(
@@ -27,27 +27,31 @@ async def _check_last_station_db_update(
                 "Therefore, starting DB population process now."
             )
             await main()
-        max_time_diff = timedelta(days=days_between_updates)
-        current_time_diff = datetime.now(tz=timezone.utc) - last_updated
-        if current_time_diff > max_time_diff:
+        else:
+            max_time_diff = timedelta(days=days_between_updates)
+            current_time_diff = datetime.now(tz=timezone.utc) - last_updated
+            if current_time_diff > max_time_diff:
+                logger.info(
+                    f"It was more than {days_between_updates} days since the "
+                    "stations DB was last updated. Therefore, starting DB population "
+                    "process now."
+                )
+                await main()
             logger.info(
-                f"It was more than {days_between_updates} days since the stations DB "
-                "was last updated. Therefore, starting DB population process now."
+                "The stations DB was last updated at "
+                f"{last_updated.strftime(settings.LOG_DT_FMT)}. Less than "
+                f"{days_between_updates} days since the last update. No action needed."
             )
-            await main()
-        logger.info(
-            "The stations DB was last updated at "
-            f"{last_updated.strftime(settings.LOG_DT_FMT)}. Less than "
-            f"{days_between_updates} days since the last update. No action needed."
-        )
 
 
-async def start_update_monitoring():
+def get_scheduler() -> BaseScheduler:
+    """Returns the scheduler."""
+    return AsyncIOScheduler()
+
+
+async def start_update_monitoring(scheduler: AsyncIOScheduler) -> None:
     """Starts the update monitoring."""
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(
-        _check_last_station_db_update, "cron", hour=3, id="check_last_station_db_update"
-    )
+    scheduler.add_job(check_last_station_db_update, "cron", hour=3)
     logger.info("Starting the stations DB update date monitoring.")
     scheduler.start()
     while True:
