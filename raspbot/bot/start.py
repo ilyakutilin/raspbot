0f1f23f18eb29803692d1bce29aa4c574228cf81
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-05-16 14:03:14 +0300

    Logging for bot/start

--- raspbot/bot/start.py
+++ raspbot/bot/start.py
@@ -29,13 +29,25 @@ async def start_command(message: types.Message):
     """User: issues /start command. Bot: please input the departure point."""
     user = await get_user_from_db(telegram_id=message.from_user.id)
     if not user:
+        logger.info(
+            f"New user detected: {message.from_user.full_name}, "
+            f"telegram id = {message.from_user.id}. Adding to DB."
+        )
         user = await create_user(tg_user=message.from_user)
+        logger.info(
+            f"User {user.full_name} TGID {user.telegram_id} issued a /start command. "
+            "Greeting new user."
+        )
         await message.answer(
             msg.GREETING_NEW_USER.format(first_name=message.from_user.first_name),
             reply_markup=keyboard,
             parse_mode="HTML",
         )
     else:
+        logger.info(
+            f"User {user.full_name} TGID {user.telegram_id} issued a /start command. "
+            "Greeting existing user."
+        )
         await message.answer(
             msg.GREETING_EXISTING_USER.format(first_name=user.first_name),
             reply_markup=keyboard,
