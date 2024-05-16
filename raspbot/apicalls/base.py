46ae04a978d9dcaadc8c5352097880cb6d11e693
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-05-16 09:17:49 +0300

    Logging for apicalls/base

--- raspbot/apicalls/base.py
+++ raspbot/apicalls/base.py
@@ -4,13 +4,14 @@ import aiohttp
 from dotenv import load_dotenv
 
 from raspbot.core import exceptions as exc
-from raspbot.core.logging import configure_logging
+from raspbot.core.logging import configure_logging, log
 
 load_dotenv()
 
 logger = configure_logging(__name__)
 
 
+@log(logger)
 async def get_response(
     endpoint: str, headers: dict[str, str | bytes | None]
 ) -> dict | None:
@@ -31,25 +32,22 @@ async def get_response(
         в противном случае возвращает None.
     """
     if headers["Authorization"] is None:
-        raise exc.EmptyHeadersError("Отсутствует ключ авторизации в хедере.")
+        raise exc.EmptyHeadersError("No authorization key in the headers.")
     async with aiohttp.ClientSession() as session:
         try:
-            logger.debug(f"Отправляю запрос на адрес {endpoint}.")
+            logger.info(f"Sending request to {endpoint}.")
             response = await session.get(url=endpoint, headers=headers)
             if response.status != HTTPStatus.OK:
                 raise exc.APIStatusCodeError(
-                    f"Адрес {endpoint} недоступен - статус: "
+                    f"{endpoint} is unavailable - status: "
                     f"{response.status} "
                     f"{HTTPStatus(response.status).phrase}. "
                 )
         except Exception as e:
             raise exc.APIConnectionError(
-                f"При соединении с эндпоинтом {endpoint} возникла ошибка. "
-                f"Хедеры: {headers}. Описание ошибки: {e}"
+                f"Error connecting to {endpoint}. "
+                f"Headers: {headers}. Error description: {e}"
             ) from e
         else:
-            logger.debug(
-                f"Запрос на эндпоинт {endpoint} был успешно осуществлен, "
-                "получен ответ."
-            )
+            logger.info(f"Request to {endpoint} has been successul, response received.")
             return await response.json(content_type=None)
