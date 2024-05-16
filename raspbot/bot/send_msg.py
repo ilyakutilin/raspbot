aa857ff8eba51e7855ed8360ff2fea3572829352
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-05-16 14:01:28 +0300

    Logging for bot/send_msg

--- raspbot/bot/send_msg.py
+++ raspbot/bot/send_msg.py
@@ -1,6 +1,9 @@
 from aiogram import Bot
 
+from raspbot.core.logging import configure_logging
 from raspbot.settings import settings
+
+logger = configure_logging(__name__)
 
 
 async def send_telegram_message_to_admin(
@@ -9,4 +12,7 @@ async def send_telegram_message_to_admin(
     bot: Bot = Bot(token=settings.TELEGRAM_TOKEN),
 ) -> None:
     """Sends a message to the chat."""
+    logger.info(
+        f"Sending message to chat {chat_id} from bot {bot.get_me().username}: {message}"
+    )
     await bot.send_message(chat_id=chat_id, text=message)
