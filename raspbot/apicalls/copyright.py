8d35d2614abf13b276fa92960d24c65b696ea041
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-05-23 16:50:49 +0300

    Get copyright from API

---
+++ raspbot/apicalls/copyright.py
@@ -0,0 +1,21 @@
+import asyncio
+from typing import Mapping
+
+from raspbot.apicalls.base import get_response
+from raspbot.core.logging import configure_logging, log
+from raspbot.settings import settings as s
+
+logger = configure_logging(name=__name__)
+
+
+@log(logger)
+async def get_copyright() -> Mapping | None:
+    """Gets the Yandex copyright."""
+    url = s.COPYRIGHT_ENDPOINT
+    headers = s.headers
+    return await get_response(endpoint=url, headers=headers)
+
+
+if __name__ == "__main__":
+    copyright = asyncio.run(get_copyright())
+    print(copyright)
